9dc5dfc22b6364f68c257303da6d4fff6708cc22
Jefferson Rodrigues <jsilvardg@outlook.com>
2022-02-20 08:39:22 -0300

    Atividades Resolvidas

---
+++ AcademiaCapgemini/Questao03.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AcademiaCapgemini;
+
+
+
+/**
+ *
+ * @author jefferson
+ */
+//Duas palavras podem ser consideradas anagramas de si mesmas se as letras de 
+//uma palavra podem ser realocadas para formar a outra palavra. Dada uma string 
+//qualquer, desenvolva um algoritmo que encontre o número de pares de substrings 
+//que são anagramas.
+
+
+public class Questao03 {
+
+
+}
